bf0aa20b5af84d7d68b4426ead3f7c8c11d57c34
n-townsend <132301374+n-townsend@users.noreply.github.com>
2023-06-19 11:23:03 -0400

    added express commands for User documentation

--- index.js
+++ index.js
@@ -1,17 +1,62 @@
 const express = require('express'); // This imports Express locally to be used in the file
     morgan = require('morgan');
+    bodyparser = require('body-parser');
+    uuid = require('uuid');
 
 const app = express(); // Declares variable encapsulating Express's functionality so it can configure web server.
 app.use(morgan('common'));
 
-let topMovies = []; // Creates object to return List of Movies, Currently Blank.
 
-app.get('/movies', (req, res) => {
-    res.json(topMovies); // Calls for the list of movies when website is directed to the /movies page.
+let movies = [];
+
+//Create User Account
+app.post('/users', (req, res) => {
+    res.send('Successful POST request sending data about New User');
 });
 
-app.get('/', (req, res) => {
-    res.send('Welcome to my Movie Catalog.');
+//Return a list of all movies
+app.get('/movies', (req, res) => {
+    res.send('Successful GET request returning data for all movies');
+});
+
+//Return data about a genre by name
+app.get('/movies/genres/:Genre', (req, res) => {
+    res.send('Successful GET request returning data for all movies based on Genre');
+});
+
+//Return data about a director by name
+app.get('/movies/directors/:Name', (req, res) => {
+    res.send('Successful GET request returning data for all Directors');
+});
+
+//Return a list of all users
+app.get('/users', (req, res) => {
+    res.send('Successful GET request returning data for all Users');
+});
+
+//Return a single user by username
+app.get('/users/:Username', (req, res) => {
+    res.send('Successful GET request returning data for specific User');
+});
+
+//Allow users to change their information
+app.put('/users/:Username', (req, res) => {
+    res.send('Successful PUT request changing user information');
+});
+
+//Allow users to add movie to favorites list
+app.post('/users/:Username/movies/:MovieID', (req, res) => {
+    res.send('Successful POST request allowing users to save movies to favorites');
+});
+
+//Allow users to remove movie from favorites list
+app.delete('/users/:Username/movies/:MovieID', (req, res) => {
+    res.send('Successful DELETE request removing movies from favorites list');
+});
+
+//Allow a user to be removed
+app.delete('/users/:Username', (req, res) => {
+    res.send('Successful DELETE request removing User from System');
 });
 
 app.use(express.static('public')); // Will route any request for static files to the correct file in the public folder
